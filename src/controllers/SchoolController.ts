51add4b1c9f8403780cce197a4132b081a2da0d5
Vinicius Rosa <vinicius.rosa00@gmail.com>
2021-02-07 23:02:54 -0300

    insert school works

--- src/controllers/SchoolController.ts
+++ src/controllers/SchoolController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import Knex from 'knex';
 import connection  from '../database/connection';
 
 class SchoolController {
@@ -7,8 +8,45 @@ class SchoolController {
     async show() {}
 
     async create(request: Request, response: Response) {
-        console.log(request)
-       response.status(200).send('ok')
+       
+        const { 
+            phone_number,
+            email,
+            complement,
+            district,
+            number,
+            street,
+            school_name 
+         } = request.body;
+
+         const trx = await connection.transaction();
+
+         const school = {
+            school_name,
+            street,
+            number,
+            district,
+            complement,
+            email,
+            phone_number,
+            latitude: 234234,
+            longitude: 234234
+         };
+
+         try {
+             const insertedSchool = await trx('schools').insert(school);
+             await trx.commit();
+
+             return response.status(200).json({insertedSchoolIs: insertedSchool[0]});
+
+         } catch(err) {
+             console.log(err);
+             await trx.rollback();
+         }
+
+
+       
+        // response.status(200).send('ok')
     }
 
     async update() {}
