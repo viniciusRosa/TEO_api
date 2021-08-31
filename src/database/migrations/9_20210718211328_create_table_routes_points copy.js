ea4b8fd3412d951de248921e9e133e92d8b73c2d
Vinicius Rosa <vinicius.rosa00@gmail.com>
2021-08-31 17:35:31 -0300

    table route_students

--- src/database/migrations/9_20210718211328_create_table_routes_points copy.js
+++ src/database/migrations/9_20210718211328_create_table_routes_points copy.js
@@ -8,7 +8,6 @@ exports.up = knex => knex.schema.createTable('routes_points', table => {
       .inTable('points'),
   table.timestamp('created_at').defaultTo(knex.fn.now()),
   table.timestamp('updated_at').defaultTo(knex.fn.now())
-
 })
 
-exports.down = knex => knex.schema.dropTable('routes')
+exports.down = knex => knex.schema.dropTable('routes_points')
